a99be7fe5c8b00d2a8e4f6651a80178b37962289
Mahadi Hasan <mahadimhs787@gmail.com>
2025-06-19 21:13:37 +0600

    completed book model

---
+++ src/app/models/book.model.ts
@@ -0,0 +1,47 @@
+import { model, Schema } from "mongoose";
+import { IBook } from "../interfaces/book.interface";
+
+const bookSchema = new Schema<IBook>({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    author: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    genre: {
+        type: String,
+        required: true,
+        enum: ['FICTION', 'NON_FICTION', 'SCIENCE', 'HISTORY', 'BIOGRAPHY', 'FANTASY'],
+        trim: true
+    },
+    isbn: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    copies: {
+        type: Number,
+        required: true,
+        min: [0, 'You must be give a positive number'],
+        validate: {
+            validator: Number.isInteger,
+            message: 'copies number must be and integer'
+        }
+    },
+    available: {
+        type: Boolean,
+        default: true
+    }
+})
+
+export const Book = model('Book', bookSchema)
